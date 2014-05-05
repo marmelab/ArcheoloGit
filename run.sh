0089263fc1266a0268921fa1fc5f41b0f829b893
Daphné Popin <daphne.popin@gmail.com>
2014-05-05 17:50:14 +0200

    Put ls-tree command in cache

--- run.sh
+++ run.sh
@@ -10,12 +10,14 @@ then
     cd $1
 fi
 
-nbFile=`git ls-tree -r --name-only HEAD | wc -l | tr -d ' '`
-echo "Number of files: $nbFile"
+tree=`git ls-tree -r --name-only HEAD`
+nbFiles=`echo "$tree" | wc -l`
+echo "Number of files: $nbFiles"
 
-git ls-tree -r --name-only HEAD | while ((i++)); read filename; do
+
+echo "$tree" | while ((i++)); read filename; do
 	echo "$(git log -1 --format="%ad" -- $filename),$(git log --oneline $filename | wc -l | tr -d ' '),$filename" >> $CSVFILE
-	percent=$(($i*100/$nbFile))
+	percent=$(($i*100/$nbFiles))
 	echo -en "\r"
 	seq  -f "=" -s '' $percent
 	echo -en ">$percent%"
