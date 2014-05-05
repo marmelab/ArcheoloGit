15118410ea783037e5cdbabeed3b37add8562605
Daphné Popin <daphne.popin@gmail.com>
2014-05-05 17:02:32 +0200

    Add progress bar on script

--- run.sh
+++ run.sh
@@ -10,6 +10,15 @@ then
     cd $1
 fi
 
-git ls-tree -r --name-only HEAD | while read filename; do
+nbFile=`git ls-tree -r --name-only HEAD | wc -l | tr -d ' '`
+echo "Number of files: $nbFile"
+
+git ls-tree -r --name-only HEAD | while ((i++)); read filename; do
 	echo "$(git log -1 --format="%ad" -- $filename),$(git log --oneline $filename | wc -l | tr -d ' '),$filename" >> $CSVFILE
+	percent=$(($i*100/$nbFile))
+	echo -en "\r"
+	seq  -f "=" -s '' $percent
+	echo -en ">$percent%"
 done
+
+echo -en "\n"
